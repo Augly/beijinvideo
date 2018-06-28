b85a55a0a5f9cd24bf2b5de5fe450659923960cf
Augly <17633369350@163.com>
2018-06-28 08:25:35 +0800

    准备着手小视频自定义视频组件

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,17 +9,25 @@ Page({
     showPlay:false,
     videoList:[
       {
-        src:'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400'
+        src:'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400',
+        imgSrc:'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1530118034&di=93140b4fdeaa8def0d2f36f341670a30&src=http://pic.58pic.com/58pic/14/34/23/26c58PIC4Pq_1024.jpg',
+        id:31,
       },
       {
-        src: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400'
+        src: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400',
+        imgSrc: 'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1530118034&di=93140b4fdeaa8def0d2f36f341670a30&src=http://pic.58pic.com/58pic/14/34/23/26c58PIC4Pq_1024.jpg',
+        id: 32,
       },
       {
-        src: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400'
+        src: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400',
+        imgSrc: 'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1530118034&di=93140b4fdeaa8def0d2f36f341670a30&src=http://pic.58pic.com/58pic/14/34/23/26c58PIC4Pq_1024.jpg',
+        id: 34,
       },
       {
-        src: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400'
-      }
+        src: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400',
+        imgSrc: 'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1530118034&di=93140b4fdeaa8def0d2f36f341670a30&src=http://pic.58pic.com/58pic/14/34/23/26c58PIC4Pq_1024.jpg',
+        id: 35,
+      },
     ]
   },
 
@@ -31,22 +39,27 @@ Page({
     
   },
   videoPlay: function (e) {
+    
     var length = this.data.videoList.length
+    var index = e.currentTarget.dataset.index
     var id = e.currentTarget.id
+    console.log(index)
     if (!this.data.playIndex) { // 没有播放时播放视频
       this.setData({
-        playIndex: id
+        playIndex: index,
+        playmid: id
       })
       var videoContext = wx.createVideoContext('myVideo' + id)
       videoContext.play()
     } else {                    // 有播放时先将prev暂停到0s，再播放当前点击的current
-      var videoContextPrev = wx.createVideoContext('myVideo' + this.data.playIndex)
+      var videoContextPrev = wx.createVideoContext('myVideo' + this.data.playmid)
       videoContextPrev.seek(0)
       videoContextPrev.pause()
       this.setData({
-        playIndex: id
+        playIndex: index,
+        playmid:id
       })
-      var videoContextCurrent = wx.createVideoContext('myVideo' + this.data.playIndex)
+      var videoContextCurrent = wx.createVideoContext('myVideo' + this.data.playmid)
       videoContextCurrent.play()
     }
   },
